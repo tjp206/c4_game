5f35f3b4a021d2ad71e71999ba5e2d5066b66afb
John McKenna <john@Johns-MacBook-Pro.local>
2020-10-25 21:45:06 +0000

    Branch of Function, First Commit 20201025

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,112 +1,88 @@
 <template>
   <div id="app">
-    <h1>C-4 Time To Go Booom!!!!</h1>
+    <div>
+      <form>
+      <form>
+        <input type="text" v-model="playerOne" id="playerOne" placeholder="Player One"/>
+        <input type="text" v-model="playerTwo" id="playerTwo" placeholder="Player Two"/>        
+      </form>
+      </form>
 
-    <h3 class="player-turn"></h3>
-
-    <div class="game">
-      <table>
-      <tr>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-      </tr>
-      <tr>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-      </tr>    
-       <tr>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-      </tr>   
-        <tr>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-      </tr>    
-      <tr>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-      </tr>    
-      <tr>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-        <td class="slot"></td>
-      </tr>
-      </table> 
     </div>
-    <div class="reset">Reset</div>
+
+    <game-function v-if="playerTwo" :playerOne="playerOne" :playerTwo="playerTwo"></game-function>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import GameFunction from './components/GameFunction'
+import { eventBus } from './main'
+import Game from './components/GameFunction'
+   var tableRow = document.getElementsByTagName('tr');
+    var tableCell = document.getElementsByTagName('td');
+    var tableSlot =  document.querySelector('.slot');
+    var playerTurn = document.querySelector('.player-turn');
+    var reset = document.querySelector('.reset')
+    var currentPlayer = 1;
+    var player1Color = 'red'
+    var player2Color = 'yellow'
+    // playerTurn.textContent = `${playerOne}'s turn!`;
 
 export default {
+  data(){
+    return {
+    playerOne:"",
+    playerTwo:"",
+    }
+  },
   name: 'App',
   components: {
-    HelloWorld
+    "game-function":Game,
   },
+
 
   mounted(){
-    let tableRow = document.getElementsByTagName('tr')
-    var tableCell = document.getElementsByTagName('td')
-    let tableSlot = document.querySelector('.slot')
-    
+    //Get Saved Player and Game Details.
+    // this.getGames();
+    // this.getPlayers();
 
-    for (let i=0; i < tableCell.length; i++){
-        tableCell[i].addEventListener('click', (e) => {
-            console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`)
-        });
-    }
-    while(!player1){
-      var player1 = prompt("Player one enter name u will be red")
-    }
-
-    while(!player2){
-      var player2 = prompt("Player one enter name you will be yellow")
-    }
-    let player1Color = 'red'
-    let player2Color = 'yellow'
+    eventBus.$on('listen', event => this.changeColor(event));
   },
 
-  method(){
-  const playerTurn = document.querySelector('.player-turn');
 
-  Array.prototype.forEach.call(tableCell, (cell) =>{
-  cell.addEventListener('click', changeColor);
-  cell.style.backgroundColor = 'white'
-})
 
-  function changeColor(event){
+  methods: {
+    what: function(){
+      console.log("hello")
+      console.log(`${event.target.parentElement.rowIndex}, ${event.target.cellIndex}`)
+    },
+
+    
+      
+        // tableCell[i].addEventListener('click', (e) => {
+        //     console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`)
+        // });
+
+    // while(!player1){
+    //   var player1 = prompt("Player one enter name u will be red")
+    //  }
+    // let player1Color = 'red'
+
+    // while(!player2){
+    //   var player2 = prompt("Player one enter name you will be yellow")
+    // }
+    // let player2Color = 'yellow'
+
+    
+
+    // Array.prototype.forEach.call(tableCell, (cell) =>{
+    // eventBus.addEventListener('listen', this.changeColor);
+    // cell.style.backgroundColor = 'white'
+    //     }
+    // )},
+
+    changeColor:  function(event){
+    console.log("changeColor")
     let column = event.target.cellIndex;
     let row = []
 
@@ -114,17 +90,18 @@ export default {
         if (tableRow[i].children[column].style.backgroundColor === 'white')
           row.push(tableRow[i].children[column])
             if (currentPlayer === 1){
+              console.log(row)
               row[0].style.backgroundColor = player1Color
               if (horizontalCheck() || verticalCheck() || diagonalCheckOne() || diagonalCheckTwo()){
                 playerTurn.textContent = `${player1} WINS!`
-                return alert (`${player1} WINNER!!! `)
+                return alert (`${playerOne} WINNER!!! `)
                 } 
                 else if(drawCheck()){
                   playerTurn.textContent = "Game is a Draw"
                   return alert ("DRAW")
                 } 
                 else{
-              playerTurn.textContent = `${player2}' turn`;
+              playerTurn.textContent = `${playerTwo}' turn`;
               return currentPlayer = 2
                 }
           }
@@ -142,97 +119,84 @@ export default {
                 else{
               playerTurn.textContent = `${player2}' turn`;
               return currentPlayer = 1
+            }
+         }
+      }
+    },
+  
+      // checks the four colours are the same
+      colorMatchCheck: function(one, two, three, four){
+        return(one === two && one === three && one === four && one !== "white")
+      },
+
+      horizontalCheck: function(){
+        for (let row=0; row< tableRow.length; row++){
+          for (let col = 0; col<4; col++){
+            if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor,
+            tableRow[row].children[col+1].style.backgroundColor,
+            tableRow[row].children[col+2].style.backgroundColor,
+            tableRow[row].children[col+3].style.backgroundColor)){
+              return true 
+            }
           }
-    }
-}
- let currentPlayer = 1;
-  playerTurn.textContent = `${player1}'s turn!`
-    
-// checks the four colours are the same
-function colorMatchCheck(one, two, three, four){
-  return(one === two && one === three && one === four && one !== "white")
-}
+        }
+      },
 
-function horizontalCheck(){
-  for (let row=0; row< tableRow.length; row++){
-    for (let col = 0; col<4; col++){
-      if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor,
-      tableRow[row].children[col+1].style.backgroundColor,
-      tableRow[row].children[col+2].style.backgroundColor,
-      tableRow[row].children[col+3].style.backgroundColor)){
-        return true 
-      }
-    }
+      verticalCheck: function(){
+        for(let col=0; col< 7; col++){
+          for(let row=0;  row<3; row ++){
+            if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor, 
+            tableRow[row+1].children[col].style.backgroundColor, 
+            tableRow[row+2].children[col].style.backgroundColor,
+            tableRow[row+3 ].children[col].style.backgroundColor,)){
+              return true;
+            }
+          }
+        }
+      },
+
+      diagonalCheckOne: function(){
+        for(let col=0; col<4; col++){
+          for(let row=0; row<3; row++){
+            if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor,
+            tableRow[row+1].children[col+1].style.backgroundColor,
+            tableRow[row+2].children[col+2].style.backgroundColor,
+            tableRow[row+3].children[col+3].style.backgroundColor)){
+              return true
+            }
+          }
+        }
+      },
+
+      diagonalCheckTwo: function(){
+        for(let col=0; col<4; col++){
+          for(let row=5; row>2; row--){
+            if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor,
+            tableRow[row-1].children[col+1].style.backgroundColor,
+            tableRow[row-2].children[col+2].style.backgroundColor,
+            tableRow[row-3].children[col+3].style.backgroundColor)){
+              return true
+            }
+          }
+        }
+      },
+
+      drawCheck: function(){
+        let fullSlot = []
+        for(i=0; i<tableCell.length; i++){
+          if(tableCell[i].style.backgroundColor !=='white'){
+            fullSlot.push(tableCell[i])
+          }
+        }
+        if(fullSlot.length === tableCell,length){
+          return true 
+        }
+      },
   }
 }
-
-function verticalCheck(){
-  for(let col=0; col< 7; col++){
-    for(let row=0;  row<3; row ++){
-      if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor, 
-      tableRow[row+1].children[col].style.backgroundColor, 
-      tableRow[row+2].children[col].style.backgroundColor,
-      tableRow[row+3 ].children[col].style.backgroundColor,)){
-        return true;
-      }
-    }
-  }
-}
-
-function diagonalCheckOne(){
-  for(let col=0; col<4; col++){
-    for(let row=0; row<3; row++){
-      if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor,
-      tableRow[row+1].children[col+1].style.backgroundColor,
-      tableRow[row+2].children[col+2].style.backgroundColor,
-      tableRow[row+3].children[col+3].style.backgroundColor)){
-        return true
-      }
-    }
-  }
-}
-
-function diagonalCheckTwo(){
-  for(let col=0; col<4; col++){
-    for(let row=5; row>2; row--){
-      if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor,
-      tableRow[row-1].children[col+1].style.backgroundColor,
-      tableRow[row-2].children[col+2].style.backgroundColor,
-      tableRow[row-3].children[col+3].style.backgroundColor)){
-        return true
-      }
-    }
-  }
-}
-
-function drawCheck(){
-  let fullSlot = []
-  for(i=0; i<tableCell.length; i++){
-    if(tableCell[i].style.backgroundColor !=='white'){
-      fullSlot.push(tableCell[i])
-    }
-  }
-  if(fullSlot.length === tableCell,length){
-    return true 
-  }
-}
-
-}
-}
-}
-
-
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}; */
 *{
   margin: 0%;
   padding: 0;
@@ -249,7 +213,7 @@ h1{
   transform: translate(-50%);
   width: 650px;
   height: 600px;
-  background: lightskyblue;
+  background: blue;
   border: 1px solid black;
   box-shadow: 7px 7px black;
   padding: 1.5rem;
--- client/src/components/GameFunction.vue
+++ client/src/components/GameFunction.vue
@@ -1,28 +1,156 @@
 <template>
-  
+<div>
+    <h1>C-4!!!!</h1>
+    <h3 class="player-turn"></h3>
+    <p>{{playerOne}} vs {{playerTwo}}</p>
+
+    
+    <p>{{tableCell}}</p>
+    <div class="game">
+      <table>
+    <!-- <button v-on:click="listen">Listen</button> -->
+      <tr>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+      </tr>
+      <tr>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+      </tr>    
+       <tr>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+      </tr>   
+        <tr>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+      </tr>    
+      <tr>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+      </tr>    
+      <tr>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+        <td class="slot"></td>
+      </tr>
+      </table>
+      <div class="reset">Reset</div>
+    </div> 
+</div>
 </template>
 
 <script>
+import { eventBus } from '../main.js'
+   var tableRow = document.getElementsByTagName('tr');
+    var tableCell = document.getElementsByTagName('td');
+    var tableSlot =  document.querySelector('.slot');
+    var playerTurn = document.querySelector('.player-turn');
+    var reset = document.querySelector('.reset')
+    
+
+
+
 export default {
-    data(){
-        return
+    name: "game-function",
+    props: ["playerOne", "playerTwo"],
+    components: {
     },
 
-mounted(){
-    let tableRow = document.getElementsByTagName('tr')
-    var tableCell = document.getElementsByTagName('td')
-    let tableSlot = document.querySelector('.slot')
-    let playerTurn = document.querySelector('.player-turn')
 
-    for (let i=0; i < tableCell.length; i++){
-        tableCell[i].addEventListener('click', (e) => {
-            console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`)
-        });
+  data(){
+    return {
+    tableRow: document.getElementsByTagName('tr'),
+    tableCell: document.getElementsByTagName('td'),
+    tableSlot:  document.querySelector('.slot'),
+    playerTurn: document.querySelector('.player-turn'),
+    reset: document.querySelector('.reset')
+    
     }
+  },
+    
+  mounted(){
+    // let currentPlayer = 1;
+    // playerTurn.textContent = `${playerOne}'s turn!`;
+
+        // for (let i=0; i < tableCell.length; i++){
+        // console.log("listeners")
+        // tableCell[i].addEventListener('click', (e) => {            
+        //     console.log(`${e.target.parentElement.rowIndex}, ${e.target.cellIndex}`)
+        // tableCell[i].addEventListener('click', changeColor);
+        // cell.style.backgroundColor = 'white'
+        // })}
+        // }
+
+        // Array.prototype.forEach.call(tableCell, (cell) =>{
+        //     eventBus.$emit('listen', (event));
+        //     cell.style.backgroundColor = 'white'
+        //             }
+        //     )}
+
+
+        for (let i=0; i < tableCell.length; i++){
+        console.log("listeners")
+        //This needs to go in!!!!!!!!
+        tableCell[i].style.backgroundColor = 'white'
+        tableCell[i].addEventListener('click', (event) => {
+            eventBus.$emit('listen', event);
+            console.log(event)
+            
+                    }
+            )}
+
+    }
+}
+    
+
+
 
     
-} 
-}
+
+
+
+    // methods: {
+
+    //     listen: function(){
+    //     console.log(tableCell)
+    //     Array.prototype.forEach.call(this.tableCell, (cell) =>{
+    //         eventBus.$emit('click', changeColor);
+    //         cell.style.backgroundColor = 'white'
+    //                 }
+    //             )}
+
+    // }
+
 </script>
 
 <style>
